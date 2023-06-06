5680d073fc4e31e0df416fdd7d4f8a3e300c2c01
Siya <del.siyamittal@gmail.com>
2023-06-06 19:49:13 +0530

    basic structure

---
+++ python-backend/animation/animated_stack.py
@@ -0,0 +1,45 @@
+from .animation_buffer import AnimationBuffer
+import pyfastrand
+from collections import deque
+class AnimatedStack:
+
+    def __init__(self):
+        self.name = "stack"
+        self.id = pyfastrand.pcg32()
+        self.contents = deque()
+        self.buffer = AnimationBuffer()
+
+    def push_back(self, element):
+        self.buffer.write(data_structure = self.name, operation="push", id = self.id, data = element)
+        self.contents.append(element)
+
+    def pop_back(self):
+        element = self.contents[-1]
+        self.buffer.write(data_structure = self.name, operation="pop", id=self.id, data = element)
+        return self.contents.pop()
+
+    def peek_back(self):
+        element = self.contents[-1]
+        self.buffer.write(data_structure=self.name, operation = "peek", id = self.id, data = element)
+        return element
+
+    def top(self):
+        return self.peek_back()
+
+    def len(self):
+        length = len(self.contents)
+        self.buffer.write(data_structure=self.name, operation="len", id = self.id, data = length)
+        return length
+    
+    def size(self):
+        return self.len()
+    
+    def is_empty(self):
+        self.buffer.write(data_structure=self.name, operation = "is_empty", id = self.id, data = not self.contents)
+        return not self.contents
+    
+    def clear(self):
+        self.buffer.write(data_structure=self.name, operation = "clear", id = self.id, data = len(self.contents))
+        self.contents.clear()
+
+
---
+++ python-backend/animation/animation_buffer.py
@@ -0,0 +1,10 @@
+class AnimationBuffer:
+
+    def __init__(self):
+        self.log = open('animation_log', 'a')
+        self.log.write("python_001\n")
+
+    def write(self, data_structure, operation, id, data):
+        self.log.write(f"{data_structure}_{id}:{operation}({data})")
+
+    
