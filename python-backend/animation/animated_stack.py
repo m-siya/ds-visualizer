832820beae7ec32694d634a69e3f28587d5d2495
Siya <del.siyamittal@gmail.com>
2023-06-07 18:26:41 +0530

    added type hints

--- python-backend/animation/animated_stack.py
+++ python-backend/animation/animated_stack.py
@@ -1,44 +1,46 @@
 from .animation_buffer import AnimationBuffer
 import pyfastrand
+from typing import Any
 from collections import deque
+
 class AnimatedStack:
 
-    def __init__(self, buffer = None):
-        self.name = "stack"
-        self.id = pyfastrand.pcg32()
-        self.contents = deque()
-        self.buffer = AnimationBuffer() if not buffer else buffer
+    def __init__(self, buffer: AnimationBuffer = None) -> None:
+        self.name: str = "stack"
+        self.id: int = pyfastrand.pcg32()
+        self.contents: deque = deque()
+        self.buffer: AnimationBuffer = AnimationBuffer() if not buffer else buffer
 
-    def push_back(self, element):
+    def push_back(self, element: Any) -> None:
         self.buffer.write(data_structure = self.name, operation="push", id = self.id, data = element)
         self.contents.append(element)
 
-    def pop_back(self):
+    def pop_back(self) -> Any:
         element = self.contents[-1]
         self.buffer.write(data_structure = self.name, operation="pop", id=self.id, data = element)
         return self.contents.pop()
 
-    def peek_back(self):
+    def peek_back(self) -> Any:
         element = self.contents[-1]
         self.buffer.write(data_structure=self.name, operation = "peek", id = self.id, data = element)
         return element
 
-    def top(self):
+    def top(self) -> Any:
         return self.peek_back()
 
-    def len(self):
+    def len(self) -> int:
         length = len(self.contents)
         self.buffer.write(data_structure=self.name, operation="len", id = self.id, data = length)
         return length
     
-    def size(self):
+    def size(self) -> int:
         return self.len()
     
-    def is_empty(self):
+    def is_empty(self) -> bool:
         self.buffer.write(data_structure=self.name, operation = "is_empty", id = self.id, data = not self.contents)
         return not self.contents
     
-    def clear(self):
+    def clear(self) -> None:
         self.buffer.write(data_structure=self.name, operation = "clear", id = self.id, data = len(self.contents))
         self.contents.clear()
 
--- python-backend/animation/animation_buffer.py
+++ python-backend/animation/animation_buffer.py
@@ -1,10 +1,10 @@
 class AnimationBuffer:
 
     def __init__(self):
-        self.log = open('animation_log', 'a')
+        self.log = open('animation.log', 'a')
         self.log.write("python_001\n")
 
-    def write(self, data_structure, operation, id, data):
+    def write(self, data_structure, operation, id, data) -> None:
         self.log.write(f"{data_structure}_{id}:{operation}({data})\n")
 
     
